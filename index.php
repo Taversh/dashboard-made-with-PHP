b3083c6680ca7379abe35a746de69591aef1ce2f
taversh <apeltavershima@gmail.com>
2024-04-13 14:27:56 +0100

    help

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+    $hello = 'Tavershima';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Dashboard</title>
+</head>
+<body>
+    <nav>
+        <div>
+            <h1>¯\_(ツ)_/¯</h1>
+        </div>
+        <div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#jump">Jump Scare</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+    <section id="about">
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ex quas dolorum quae nobis maxime quod velit voluptatibus accusamus necessitatibus aliquid, saepe aperiam repellat quibusdam aliquam facilis nulla cumque voluptatem?
+        Nihil esse, ratione, debitis recusandae quae amet rerum veritatis provident voluptate eius omnis rem dolorum similique culpa animi iure incidunt, alias exercitationem natus! Veritatis itaque commodi quo, explicabo repudiandae exercitationem.
+        Vero, enim nostrum beatae soluta nemo consectetur hic minus voluptatem quibusdam omnis impedit perferendis quae, ipsa ad, sed aliquid expedita dolorum rerum sunt exercitationem veritatis fugit laborum nobis reiciendis. Cumque.</p>
+        <p><?php echo"$hello" ?></p>
+    </section>
+    <section id="services">
+        <h1>Services</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ut pariatur iure ullam error necessitatibus, exercitationem autem voluptatibus voluptates accusamus distinctio eos tempora eius temporibus quo repellat in hic et. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste quis quam repudiandae consequuntur tempore vel delectus? Nisi eum error modi commodi magni eligendi omnis sed, adipisci veniam nobis saepe accusamus?
+        Laborum voluptatum dolorum dolore numquam fuga ratione libero. Debitis dolorum, vel minus eius saepe distinctio, iure adipisci illum laboriosam dolore accusamus unde delectus? Inventore atque veniam enim ut perspiciatis autem!</p>
+    </section>
+    <section id="jump">
+        <h1>Jump Scares</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi tempore iusto error sed porro. Possimus saepe id vero ab amet, quasi iure optio dolor, officiis aperiam cumque laudantium eius assumenda.
+        Quaerat, vitae ipsum deleniti maxime molestias qui ipsam fuga vero voluptatem tenetur temporibus nulla. Odio suscipit rerum praesentium molestiae explicabo vero amet vitae mollitia possimus nesciunt, ad, pariatur dolores sed!
+        Itaque ipsa non maxime incidunt numquam voluptatum ullam illo recusandae, dolorum rem. Labore officiis eos dolores laudantium at possimus libero veritatis non doloribus, cupiditate eveniet. Adipisci expedita sunt voluptas temporibus.</p>
+    </section>
+    <section id="contact">
+        <h1>Contact</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et veniam assumenda, ex aspernatur nesciunt repellat sapiente praesentium suscipit sed. Numquam aperiam nostrum impedit libero ullam ratione est maxime perferendis voluptatem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae quidem reiciendis, pariatur aut facere ex et consectetur dolorum eos, ipsa minus! At voluptatem est dolorum sit soluta. Modi, voluptatum totam? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab libero esse voluptas quisquam, delectus dolorem voluptatum, maiores dignissimos nesciunt vel impedit magnam unde? Facere beatae aliquam nostrum molestias, eius illum.</p>
+    </section>
+    <a href="#">
+        <div class="to-the-top">
+            &UpTeeArrow;
+        </div>
+    </a>
+</body>
+</html>
